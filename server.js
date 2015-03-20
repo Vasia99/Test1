0471986d7fd3e0b2848246d5fcbd7817044c8540
edwardntn <edward.ntn@gmail.com>
2015-03-20 18:58:08 +0200

    Second commit after changing in var

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  * Created by ProBook on 18.03.2015.
  */
 var user=require('./user/index.js');
-var vasya=new user.User("Vasia");
+var vasya=new user.User("Vasia99");
 var petya=new user.User("Petia");
 vasya.hello(vasya);
 var sum=require('./operation.js');
